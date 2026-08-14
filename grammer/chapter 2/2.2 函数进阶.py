d75b2b30de0cb643ba48d2bda0f17ef42f20b706
Tianqi <145124913+mstianqi@users.noreply.github.com>
2026-08-14 19:06:22 +0800

    8.14 fifth

--- grammer/chapter 2/2.2 函数进阶.py
+++ grammer/chapter 2/2.2 函数进阶.py
@@ -25,7 +25,34 @@
 #     return oper(a,b)
 # print(calc(1,2,add))
 
-# 匿名函数。根据每一个元素的字符个数排序
-data_list = ["abc", "ab", "abcd", "a"]
-data_list.sort(key = lambda x: len(x), reverse = True)
-print(data_list)
+# # 匿名函数。根据每一个元素的字符个数排序
+# data_list = ["abc", "ab", "abcd", "a"]
+# data_list.sort(key = lambda x: len(x), reverse = True)
+# print(data_list)
+
+# # 类型注解（不指定解释器会自动进行类型推断）
+# a: int = 10
+# b: float = 3.141
+# c: str = "abc"
+
+# # 商品价格=基础价格-优惠券+运费。商品种类不唯一。优惠券5000以上才能用
+# def calc_order_cost(*args, coupon=0, express=0.0):
+#     total_price = [goods[1] * goods[2] for goods in args]
+#     total_cost = sum(total_price)
+#     if total_cost >= 5000:
+#         total_cost -= coupon
+#     total_cost += express
+#     return total_cost
+# total = calc_order_cost(("鼠标", 100, 2),("键盘", 200, 3),("手机", 5000, 1), coupon=1000, express=0.2)
+# print(total)
+
+# 函数的类型注解
+def calc_order_cost(*args:tuple[str,int,int], coupon:int=0, express:float=0.0) -> float:
+    total_price = [goods[1] * goods[2] for goods in args]
+    total_cost = sum(total_price)
+    if total_cost >= 5000:
+        total_cost -= coupon
+    total_cost += express
+    return total_cost
+total = calc_order_cost(("鼠标", 100, 2),("键盘", 200, 3),("手机", 5000, 1), coupon=1000, express=0.2)
+print(total)
